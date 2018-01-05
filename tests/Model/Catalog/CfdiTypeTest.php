ec73ac3f8895bf34733e0af437af3e02ff3da574
Javier Spagnoletti <phansys@gmail.com>
2018-01-05 15:28:26 -0300

    Add some tests

---
+++ tests/Model/Catalog/CfdiTypeTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of Facturama PHP SDK.
+ *
+ * (c) Facturama <dev@facturama.com>
+ *
+ * This source file is subject to a MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Facturama\Tests\Model\Catalog;
+
+use Facturama\Client;
+use Facturama\Model\Catalog\CfdiType;
+use Facturama\Tests\FacturamaBaseTest;
+
+/**
+ * @author Javier Spagnoletti <phansys@gmail.com>
+ */
+class CfdiTypeTest extends FacturamaBaseTest
+{
+    public function testCfdiTypeApi()
+    {
+        $this->assertNotEmpty(\stdClass::class, $this->client->get('catalogs/CfdiTypes'));
+    }
+
+    /**
+     * @dataProvider getCfdiTypes
+     */
+    public function testCfdiTypes(\stdClass $cfdiType)
+    {
+        $this->assertObjectHasAttribute('Name', $cfdiType);
+        $this->assertSame($cfdiType->Name, CfdiType::findByName($cfdiType->Name)['name']);
+
+        $this->assertObjectHasAttribute('Value', $cfdiType);
+        $this->assertSame($cfdiType->Value, CfdiType::findByValue($cfdiType->Value)['value']);
+    }
+
+    public function testNoUnusedCfdiTypes()
+    {
+        $apiCfdiTypes = $this->getCfdiTypes();
+        $cfdiTypes = CfdiType::CATALOG_CFDI_TYPE;
+
+        foreach ($apiCfdiTypes as $cfdiType) {
+            $cfdiType = reset($cfdiType);
+            if (false === $index = array_search($cfdiType->Name, array_column(CfdiType::CATALOG_CFDI_TYPE, 'name'), true)) {
+                $this->fail(sprintf('CFDI type with name "%s" was not found', $cfdiType->Name));
+            }
+
+            unset($cfdiTypes[$index]);
+        }
+
+        $this->assertEmpty($cfdiTypes);
+    }
+
+    /**
+     * return \stdClass[]|\Generator
+     */
+    public function getCfdiTypes()
+    {
+        $client = new Client(getenv('api_username'), getenv('api_password'));
+
+        foreach ($client->get('catalogs/CfdiTypes') as $cfdiType) {
+            yield [$cfdiType];
+        }
+    }
+}
---
+++ tests/Model/Catalog/PaymentMethodTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of Facturama PHP SDK.
+ *
+ * (c) Facturama <dev@facturama.com>
+ *
+ * This source file is subject to a MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Facturama\Tests\Model\Catalog;
+
+use Facturama\Client;
+use Facturama\Model\Catalog\PaymentMethod;
+use Facturama\Tests\FacturamaBaseTest;
+
+/**
+ * @author Javier Spagnoletti <phansys@gmail.com>
+ */
+class PaymentMethodTest extends FacturamaBaseTest
+{
+    public function testPaymentMethodApi()
+    {
+        $this->assertNotEmpty(\stdClass::class, $this->client->get('catalogs/PaymentMethods'));
+    }
+
+    /**
+     * @dataProvider getPaymentMethods
+     */
+    public function testPaymentMethods(\stdClass $paymentMethod)
+    {
+        $this->assertObjectHasAttribute('Name', $paymentMethod);
+        $this->assertSame($paymentMethod->Name, PaymentMethod::findByName($paymentMethod->Name)['name']);
+
+        $this->assertObjectHasAttribute('Value', $paymentMethod);
+        $this->assertSame($paymentMethod->Value, PaymentMethod::findByValue($paymentMethod->Value)['value']);
+    }
+
+    public function testNoUnusedPaymentMethods()
+    {
+        $apiPaymentMethods = $this->getPaymentMethods();
+        $paymentMethods = PaymentMethod::CATALOG_PAYMENT_METHOD;
+
+        foreach ($apiPaymentMethods as $paymentMethod) {
+            $paymentMethod = reset($paymentMethod);
+            if (false === $index = array_search($paymentMethod->Name, array_column(PaymentMethod::CATALOG_PAYMENT_METHOD, 'name'), true)) {
+                $this->fail(sprintf('Payment method with name "%s" was not found', $paymentMethod->Name));
+            }
+
+            unset($paymentMethods[$index]);
+        }
+
+        $this->assertEmpty($paymentMethods);
+    }
+
+    /**
+     * return \stdClass[]|\Generator
+     */
+    public function getPaymentMethods()
+    {
+        $client = new Client(getenv('api_username'), getenv('api_password'));
+
+        foreach ($client->get('catalogs/PaymentMethods') as $paymentMethod) {
+            yield [$paymentMethod];
+        }
+    }
+}
