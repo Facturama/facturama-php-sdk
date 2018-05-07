5a4f389bbea2222369252c9a8819b6daefa3eae1
MariaFernandaZuleta <fernandazub28@gmail.com>
2018-05-07 13:28:32 -0500

    se cambio el catalogo de Unit

--- src/Model/Catalog/UnitCatalog.php
+++ src/Model/Catalog/UnitCatalog.php
@@ -1,23 +1,13 @@
 <?php
 
-
-require __DIR__.'/../../../vendor/autoload.php';
-
-
-printf('<pre>%s<pre>', var_export($result, true));
-
-    public static function findByValue($value,)
-    {
+	require __DIR__.'/../../../vendor/autoload.php';
 		$facturama = new Facturama\Client("pruebas", "pruebas2011");
 		$params = [
-		'keyword'=> ''.$value;
+		'keyword'=> ''."18"
 		];
 		$result = $facturama->get('Catalogs/Units', $params);
 		$x=$result["0"];
 		$y=$x->Value;
-		printf('<pre>%s<pre>', var_export($result, true));
 
 
-
-        return $result;
-    }
+return $y;
