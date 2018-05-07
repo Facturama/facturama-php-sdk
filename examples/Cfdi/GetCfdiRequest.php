dd4dfca7dc003829983b297d0baed5bcaa416542
MariaFernandaZuleta <fernandazub28@gmail.com>
2018-05-07 11:38:06 -0500

    se agrego el ejemplo de enviar factura

---
+++ examples/Cfdi/GetCfdiRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of Facturama PHP SDK.
+ *
+ * (c) Facturama <dev@facturama.com>
+ *
+ * This source file is subject to a MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require __DIR__.'/../../vendor/autoload.php';
+
+$facturama = new Facturama\Client('pruebas', 'pruebas2011');
+
+$CfdiId = 'sniJoQdMCaa1c3M_7TLjIQ2';
+
+$params = [
+	'type' => 'issued'
+];
+
+$result = $facturama->get('Cfdi/'.$CfdiId, $params);
+
+
+printf('<pre>%s<pre>', var_export($result, true));
---
+++ examples/Cfdi/PostSendEmailRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Facturama PHP SDK.
+ *
+ * (c) Facturama <dev@facturama.com>
+ *
+ * This source file is subject to a MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require __DIR__.'/../../vendor/autoload.php';
+
+$facturama = new Facturama\Client("pruebas", "pruebas2011");
+$body= [];
+$params = [
+'cfdiType' => 'issued',
+'cfdiId' => '7eo51BvzV-E16gBx3nnxfQ2',
+'email' => 'fernandazub28@gmail.com',
+
+];
+
+
+
+$result = $facturama->post('Cfdi',$body, $params);
+
+printf('<pre>%s<pre>', var_export($result, true));
